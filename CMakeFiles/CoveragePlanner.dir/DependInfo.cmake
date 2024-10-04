b2e6ccdd07eb358e4a6471c589bacdbc9e463e06
kwokkeith <keithkwok99@gmail.com>
2024-10-04 16:33:57 +0800

    Fix cmakelist

--- CMakeFiles/CoveragePlanner.dir/DependInfo.cmake
+++ CMakeFiles/CoveragePlanner.dir/DependInfo.cmake
@@ -1,30 +1,35 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/bcd.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/bcd.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/cgal_comm.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/cgal_comm.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/decomposition.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/decomposition.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/main.cpp" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/main.cpp.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/sweep.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/sweep.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/tcd.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/tcd.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/visibility_graph.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/visibility_graph.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/visibility_polygon.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/visibility_polygon.cc.o"
+  "/home/keith/Documents/Capstone/Tools/CoveragePlanner/src/weakly_monotone.cc" "/home/keith/Documents/Capstone/Tools/CoveragePlanner/CMakeFiles/CoveragePlanner.dir/src/weakly_monotone.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "CGAL_USE_CORE=1"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/bcd.cc" "CMakeFiles/CoveragePlanner.dir/src/bcd.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/bcd.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/cgal_comm.cc" "CMakeFiles/CoveragePlanner.dir/src/cgal_comm.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/cgal_comm.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/decomposition.cc" "CMakeFiles/CoveragePlanner.dir/src/decomposition.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/decomposition.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/main.cpp" "CMakeFiles/CoveragePlanner.dir/src/main.cpp.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/main.cpp.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/sweep.cc" "CMakeFiles/CoveragePlanner.dir/src/sweep.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/sweep.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/tcd.cc" "CMakeFiles/CoveragePlanner.dir/src/tcd.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/tcd.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/visibility_graph.cc" "CMakeFiles/CoveragePlanner.dir/src/visibility_graph.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/visibility_graph.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/visibility_polygon.cc" "CMakeFiles/CoveragePlanner.dir/src/visibility_polygon.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/visibility_polygon.cc.o.d"
-  "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/src/weakly_monotone.cc" "CMakeFiles/CoveragePlanner.dir/src/weakly_monotone.cc.o" "gcc" "CMakeFiles/CoveragePlanner.dir/src/weakly_monotone.cc.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/include/opencv4"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner
+# Install script for directory: /home/keith/Documents/Capstone/Tools/CoveragePlanner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,14 +27,14 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/pe3nu7/Documents/SUTD/Capstone/Codes/CoveragePlanner/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/keith/Documents/Capstone/Tools/CoveragePlanner/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
